15bb9e05a0bd897cb30e337d8e81b62314c873af
cpascuas <59749784+cpascuas@users.noreply.github.com>
2020-11-15 21:59:06 -0500

    checkout and subtotal added

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
+// import Login from ".Login";
 
 function App() {
   return (
-    <div className="app">
-      <Router>
+    <Router>
+      <div className="app">
         <Switch>
-          <Route path="/login">
-            <Login />
+          <Route path="/checkout">
+            <Header />
+            <h1> i am checkout </h1>
+          </Route>
+          <Route path="/">
             <Header />
             <Home />
           </Route>
         </Switch>
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
